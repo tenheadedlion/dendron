b19bc9bd4b0fd6418ab6839db11b906cc8126ee0
Jonathan Yeung <jonathanyeung@outlook.com>
2022-05-11 17:43:50 +0800

    spike: init draft revision of backlinks panel with hover

--- packages/plugin-core/src/workspace.ts
+++ packages/plugin-core/src/workspace.ts
@@ -581,6 +581,7 @@ export class DendronExtension implements IDendronExtension {
 
     const backlinksTreeDataProvider = new BacklinksTreeDataProvider(
       this.getEngine(),
+      this.getEngine(),
       this.getDWorkspace().config.dev?.enableLinkCandidates
     );
 
